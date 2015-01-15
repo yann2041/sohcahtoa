4be75028073e96f3f17b122c82c21e9709b855a2
Yann BENOIST <yann-benoist@hotmail.fr>
2015-01-15 18:01:56 +0100

    Ajout d'une interface graphique en WPF

--- Sohcahtoa.View/MainWindow.xaml.cs
+++ Sohcahtoa.View/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sohcahtoa.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sohcahtoa.View
 {
@@ -22,6 +10,7 @@ namespace Sohcahtoa.View
   {
     public MainWindow()
     {
+      DataContext = new SohcahtoaViewModel();
       InitializeComponent();
     }
   }
---
+++ Sohcahtoa.ViewModel/SohcahtoaViewModel.cs
@@ -0,0 +1,73 @@
+﻿using GalaSoft.MvvmLight;
+using Sohcahtoa.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sohcahtoa.ViewModel
+{
+    public class SohcahtoaViewModel : ViewModelBase
+    {
+      private TriangleRectangle _triangle = new TriangleRectangle(SocatoahToolbox.DegreesToRadians(30.0), 10.0);
+
+      public double Angle
+      {
+        get
+        {
+          return SocatoahToolbox.RadiansToDegrees(_triangle.Angle);
+        }
+        set
+        {
+          _triangle.Angle = SocatoahToolbox.DegreesToRadians(value);
+          RaisePropertyChanged("Angle");
+          RaisePropertyChanged("Hypotenuse");
+          RaisePropertyChanged("Oppose");
+        }
+      }
+       
+      public double Hypotenuse
+        {
+          get
+          {
+            return _triangle.Hypotenuse;
+          }
+          set
+          {
+            _triangle.Hypotenuse = value;
+            RaisePropertyChanged("Hypotenuse");
+            RaisePropertyChanged("Adjacent");
+            RaisePropertyChanged("Oppose");
+          }
+        }
+      public double Oppose
+      {
+        get
+        {
+          return _triangle.Oppose;
+        }
+        set
+        {
+          _triangle.Oppose = value;
+          RaisePropertyChanged("Oppose");
+          RaisePropertyChanged("Hypotenuse");
+          RaisePropertyChanged("Angle");
+        }
+      }
+      public double Adjacent
+      {
+        get
+        {
+          return _triangle.Adjacent;
+        }
+        set
+        {
+          _triangle.Adjacent = value;
+          RaisePropertyChanged("Adjacent");
+          RaisePropertyChanged("Oppose");
+          RaisePropertyChanged("Hypotenuse");
+        }
+      }
+    }
+}
